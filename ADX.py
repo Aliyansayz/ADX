8bfea2768c6667aadc497761c9244ccada1f9f75
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-04-26 18:33:50 +0500

    Update ADX.py

--- ADX.py
+++ ADX.py
@@ -19,16 +19,16 @@ def sma (array, period ):
     
     sma = np.empty_like(array)
     sma = np.full( sma.shape , np.nan)
-    # Calculate the EMA for each window of 14 values
+    
     for i in range(period, len(array)+1 ):
           sma[i-1] = np.mean(array[i-period:i] , dtype=np.float16)
     return sma 
 
-def adx_calc(dx , period ):
+def calc(dx , period ):
     adx = np.empty_like(dx)
     adx = np.full( dx.shape , np.nan)
     adx[period-1] = np.mean(dx[:period] , dtype=np.float16)
-    # Calculate the EMA for each window of 14 values
+    
     for i in range(period, len(dx)  ):
           adx[i] = np.array( (adx[i-1]*13 + dx[i] )/ period , dtype=np.float16)
     return adx 
@@ -39,46 +39,48 @@ def true_range( bar , period  ):
     np.array(abs(bars["High"]-bars["Close"].shift()),dtype=np.float16 ) , 
     np.array(abs(bars["Low"]-bars["Close"].shift() ),dtype=np.float16 )
 
-    true_range = np.amax (np.hstack( (high_low, high_close, low_close) ).reshape(-1,3),axis=1 )  
+    true_range = np.amax (np.hstack( (high_low, high_close, low_close) ).reshape(-1,3),axis=1 )
+    true_range = np.nan_to_num(true_range , nan=0) 
+
     return true_range 
 
 
 def smoothed(self, array, period , alpha = None):
     ema = np.empty_like(array)
-    ema = np.full( ema.shape , np.nan)
+    ema = np.full( array.shape , np.nan)
     ema[0] = np.mean(array[0] , dtype=np.float16)
     if alpha == None:
       alpha = 1 / ( period )
-    # Calculate the EMA for each window of 14 values
+    
     for i in range(1 , len(array) ):
           ema[i] = np.array( (array[i] * alpha +  ema[i-1]  * (1-alpha) ) , dtype=np.float16 )
     return ema 
 
-def adx_calc( dx , period ):
-    adx = np.empty_like(dx)
-    adx = np.full( dx.shape , np.nan)
-    adx[period-1] = sum( dx[:period] ) / period
-    # Calculate the EMA for each window of 14 values
-    for i in range(period, len(dx)  ):
-          adx[i] = (( adx[i-1]*13 ) + dx[i] ) / period 
-    return adx 
 
-def  ADX(bar period ):
-    import numpy as np
-    true_range  = self.true_range(bar , period )
-    high = np.array( bar.High , dtype = np.float16 )
-    low = np.array( bar.Low , dtype = np.float16
-    highs , lows =   high - shift(high , 1 ) ,  shift(low , 1) - low 
-    
-    pdm = np.where(highs > lows  , abs(highs) , 0 )
-    ndm = np.where(lows  > highs , abs(lows) , 0  )
-        # avg_true_range = self.sma(true_range , period )  
+  def calc( array , period):
+        adx = np.empty_like(array)
+        adx = np.full( array.shape , np.nan)
+        adx[period-1] = sum( array[:period] ) / period
 
-    atr  = sma(true_range , period)
-    pdi = ( smoothed( pdm , period)  / atr ) * 100 
-    ndi = ( smoothed( ndm , period) / atr ) * 100
-    dx = ( abs(pdi - ndi) ) / ( abs(pdi + ndi) ) * 100 
-    adx =   sma( dx  , period ) 
-    # adx = self.smoothed( adx , period) 
+        for i in range(period, len(array)) :
+              adx[i] = (( adx[i-1]*13 ) + array[i] ) / period 
+        return adx 
 
-    return adx
+  def  ADX(bar period ):
+        import numpy as np
+        true_range  = self.true_range(bar , period )
+        high = np.array( bar.High , dtype = np.float16 )
+        low = np.array( bar.Low , dtype = np.float16
+        highs , lows =   high - shift(high , 1 ) ,  shift(low , 1) - low 
+
+        pdm = np.where(highs > lows  , abs(highs) , 0 )
+        ndm = np.where(lows  > highs , abs(lows) , 0  )
+            
+        atr  = sma(true_range , period)
+        pdi = ( smoothed( pdm , period)  / atr ) * 100 
+        ndi = ( smoothed( ndm , period) / atr ) * 100
+        dx = ( abs(pdi - ndi) ) / ( abs(pdi + ndi) ) * 100 
+        dx = np.nan_to_num(dx , nan=0)
+        adx = calc( dx , period)
+
+        return adx

76ea5b63f6750343043e99189cb696137837daf1
Aliyan Anwar <56589240+Aliyansayz@users.noreply.github.com>
2023-04-26 15:20:20 +0500

    Update ADX.py

--- ADX.py
+++ ADX.py
@@ -7,26 +7,22 @@ bar = yfinance or inclusive bar["High"]  bar["Low"] bar["Close"]
 adx    =   ADX(bar , period = 14 )
 """
 
+def shift(self, array , place):
+    array = np.array(array , dtype= np.float16 )
+    array =  array.astype(np.float16)
+    shifted = np.roll(array, place)
+    shifted[0:place] = np.nan
+
+    return shifted
 
 def sma (array, period ):
-
+    
     sma = np.empty_like(array)
     sma = np.full( sma.shape , np.nan)
     # Calculate the EMA for each window of 14 values
     for i in range(period, len(array)+1 ):
           sma[i-1] = np.mean(array[i-period:i] , dtype=np.float16)
     return sma 
-
-def ema (array, period ):
-
-    ema = np.empty_like(array)
-    ema = np.full( ema.shape , np.nan)
-    ema[0] = np.mean(array[0] , dtype=np.float16)
-    alpha = 2 / (period + 1)
-    # Calculate the EMA for each window of 14 values
-    for i in range(1 , len(array) ):
-          ema[i] = np.array( (array[i] * alpha +  ema[i-1]  * (1-alpha) ) , dtype=np.float16 )
-    return ema 
 
 def adx_calc(dx , period ):
     adx = np.empty_like(dx)
@@ -37,34 +33,54 @@ def adx_calc(dx , period ):
           adx[i] = np.array( (adx[i-1]*13 + dx[i] )/ period , dtype=np.float16)
     return adx 
 
-
 def true_range( bar , period  ):
   
-  high_low, high_close, low_close  = np.array(bars["High"]-bars["Low"],dtype=np.float16 ) , 
-  np.array(abs(bars["High"]-bars["Close"].shift()),dtype=np.float16 ) , 
+    high_low, high_close, low_close  = np.array(bars["High"]-bars["Low"],dtype=np.float16 ) , 
+    np.array(abs(bars["High"]-bars["Close"].shift()),dtype=np.float16 ) , 
     np.array(abs(bars["Low"]-bars["Close"].shift() ),dtype=np.float16 )
+
+    true_range = np.amax (np.hstack( (high_low, high_close, low_close) ).reshape(-1,3),axis=1 )  
+    return true_range 
+
+
+def smoothed(self, array, period , alpha = None):
+    ema = np.empty_like(array)
+    ema = np.full( ema.shape , np.nan)
+    ema[0] = np.mean(array[0] , dtype=np.float16)
+    if alpha == None:
+      alpha = 1 / ( period )
+    # Calculate the EMA for each window of 14 values
+    for i in range(1 , len(array) ):
+          ema[i] = np.array( (array[i] * alpha +  ema[i-1]  * (1-alpha) ) , dtype=np.float16 )
+    return ema 
+
+def adx_calc( dx , period ):
+    adx = np.empty_like(dx)
+    adx = np.full( dx.shape , np.nan)
+    adx[period-1] = sum( dx[:period] ) / period
+    # Calculate the EMA for each window of 14 values
+    for i in range(period, len(dx)  ):
+          adx[i] = (( adx[i-1]*13 ) + dx[i] ) / period 
+    return adx 
+
+def  ADX(bar period ):
+    import numpy as np
+    true_range  = self.true_range(bar , period )
+    high = np.array( bar.High , dtype = np.float16 )
+    low = np.array( bar.Low , dtype = np.float16
+    highs , lows =   high - shift(self.high , 1 ) ,  shift(self.low , 1) - low 
     
-  true_range = np.amax (np.hstack( (high_low, high_close, low_close) ).reshape(-1,3),axis=1 )  
-  return true_range 
+    pdm = np.where(highs > lows  , abs(highs) , 0 )
+    ndm = np.where(lows  > highs , abs(lows) , 0  )
+        # avg_true_range = self.sma(true_range , period )  
+    pdm_smoothed = smoothed( pdm , period)
+    ndm_smoothed = smoothed( ndm , period)
 
-def  ADX(bar , period ):
-  
-  true_range  = true_range(bar , multiplier)
-  highs , lows =   np.array(abs(bar["High"] - bar["High"].shift(1)), dtype=np.float16)  
-  , np.array(abs(bar["Low"].shift(1) - bar["Low"]), dtype=np.float16 )
-  condition_pdm = highs > lows
-  condition_ndm = lows  > highs 
-  pdm = np.where(condition_pdm, highs , 0.0 )
-  ndm = np.where(condition_ndm, lows , 0.0 )
+    atr  = sma(true_range , period)
+    pdi = ( smoothed( pdm , period)  / atr ) * 100 
+    ndi = ( smoothed( ndm , period) / atr ) * 100
+    dx = ( abs(pdi - ndi) ) / ( abs(pdi + ndi) ) * 100 
+    adx =   sma( dx  , period ) 
+    # adx = self.smoothed( adx , period) 
 
-  pdm , ndm = sma(pdm , period ) , sma(ndm , period)
-  pdm_smoothed = ema(pdm , period)
-  ndm_smoothed = ema(ndm , period)
-  tr_smoothed  = ema(true_range , period)
-
-  pdi = ( pdm_smoothed / tr_smoothed ) * 100
-  ndi = ( ndm_smoothed / tr_smoothed ) * 100
-
-  dx = ( abs(pdi - ndi) ) / ( abs(pdi + ndi) ) * 100
-  adx =   adx_calc(dx , period )
-  return adx
+    return adx
